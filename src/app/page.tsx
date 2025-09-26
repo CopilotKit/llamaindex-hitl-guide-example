3576395866db4467ad5df66bd56dc3f3f97f317e
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-09-26 18:20:33 -0400

    chore: remove weather references and related component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { WeatherCard } from "@/components/WeatherCard";
 
 import { useCoAgent, useCopilotAction } from "@copilotkit/react-core";
 import { Markdown } from "@copilotkit/react-ui"
